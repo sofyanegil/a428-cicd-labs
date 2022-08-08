060f3da8af00613f727972624d2e47b0fe2609cf
Sofyan Egi <sofyanegil@gmail.com>
2022-08-08 13:06:50 +0700

    Add Name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React!</h1>
+          <h2>Sofyan Egi Lesmana</h2>
+          <p>Submission Proyek Membangun CI Pipeline dengan Jenkins</p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
