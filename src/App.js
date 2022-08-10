27f3b0682c6aeb32f5eb7af325fe8eea2f07dce2
Sofyan Egi <sofyanegil@gmail.com>
2022-08-10 15:06:03 +0700

    Welcome to React !!!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Welcome to React!!!</h1>
           <h2>Sofyan Egi Lesmana</h2>
           <p>Submission Proyek Membangun CI Pipeline dengan Jenkins</p>
         </header>
